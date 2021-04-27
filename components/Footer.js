29a48eff0146687778de9aaa06e0dff50041cf47
biaksangmunsong <biaksangmunsong@gmail.com>
2021-04-27 21:07:23 +0530

    developer's link updated

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ const Footer = ({ discordLink }) => {
     return (
         <FooterWrapper>
             <section>
-                <p className="designer">Designed by <a href="https://github.com/biaksangmunsong" target="_blank" rel="noopener noreferrer">Biaksang Munsong</a></p>
+                <p className="designer">Designed by <a href="https://biaksang.in" target="_blank" rel="noopener noreferrer">Biaksang Munsong</a></p>
                 <a href={ discordLink } className="discord_"><Icon id="discord" /></a>
                 <button type="button" className="back-to-top" onClick={ () => window.scrollTo(0, 0) }><Icon id="up" /></button>
             </section>
